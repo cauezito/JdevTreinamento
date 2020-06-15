09326c4fed3cfc51eea4aca3ca31d7646ee899c3
Cauê Santos <caah.sfc@live.com>
2020-06-15 20:29:53 -0300

    Correção na validação do formulário de usuários

--- Projeto JSP/WebContent/User/js/newUser.js
+++ Projeto JSP/WebContent/User/js/newUser.js
@@ -1,14 +1,12 @@
 function validateForm(){
 	let form = document.getElementById("formUser");
-	var isValid = true;
+	let isValid = true;
 	 [...form.elements].forEach(function(field){
-         if(['name', 'lastName', 'login', 'password'].indexOf(field.name) > -1 && !field.value){
+         if(['name', 'lastName', 'login', 'password'].indexOf(field.name) > -1 && !field.value || field.value.indexOf(' ') >= 0){
              field.classList.add('invalid');
              isValid = false;
-         }        
-         
-         return isValid;
-     });
+         } 
+     });	
 	 
-	 
+	 return isValid;
 }
